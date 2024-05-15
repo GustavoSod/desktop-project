15dc39d08c503ff6b7d0138784ca2c6c2a5feaff
Gustavo Sodré <gustavo.sodre.carreira.rosa@gmail.com>
2024-05-15 20:36:25 -0300

    first commit

---
+++ src/main/java/com/mycompany/projetofaculdade/ProjetoFaculdade.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetofaculdade;
+
+/**
+ *
+ * @author gustavosodre
+ */
+public class ProjetoFaculdade {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
